cdde6b5d4a55fb392f66c47a96b927208937e26d
jerhum <jerhum@msn.com>
2024-12-05 15:32:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./app.css";
+import Accueil from "./pages/Accueil/Accueil";
+
+function App() {
+  return (
+    <div className="appContainer">
+      <Accueil />
+    </div>
+  );
+}
+
+export default App;
